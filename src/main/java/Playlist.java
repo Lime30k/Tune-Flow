28e1ea7ace917513a694b9e7658f1c6d1967a9db
Linus <Linus.meyerhoff@student.evbg.de>
2025-05-26 13:08:52 +0200

    tjifdosa

---
+++ src/main/java/Playlist.java
@@ -0,0 +1,5 @@
+public class Playlist {
+    String name;
+    long  length;
+
+}
---
+++ src/main/java/Song.java
@@ -0,0 +1,34 @@
+public class Song {
+    String name;
+    String genre;
+    double lenght;
+    String artist;
+    int likes;
+    String filepath;
+
+    public Song(){
+
+    }
+    public String getName(){
+        return name;
+    }
+    public String getGenre(){
+        return genre;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public double getLenght() {
+        return lenght;
+    }
+}
