4e4294eaef061c82de41823d07544d2428869f3c
Andreas <Andreas.Lenz@student.evbg.de>
2025-05-28 08:55:34 +0200

    g

--- src/main/java/com/example/walter/User.java
+++ src/main/java/com/example/walter/User.java
@@ -1,10 +1,14 @@
 package com.example.walter;
-
+import java.util.Arrays;
 public class User {
     String username;
     int ID;
+    Playlist[] playlists;
     public void User(String name){
         username=name;
         ID=1;
+    }
+    public void createPlaylist(){
+
     }
 }
