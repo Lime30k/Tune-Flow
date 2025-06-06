cc40dbdedba1b13d5957bfaed0907b4e38bbd019
Lime30k <meyerhoff.l@outlook.de>
2025-06-06 19:30:29 +0200

    Playlist formated

--- src/main/java/com/example/walter/Playlist.java
+++ src/main/java/com/example/walter/Playlist.java
@@ -3,15 +3,14 @@ package com.example.walter;
 import java.util.ArrayList;
 
 public class Playlist {
-public String name;
-ArrayList<Song> playlist;
-public Playlist(String n)
-{
-    playlist=new ArrayList<Song>();
-    name=n;
-}
-public void Play(int Index){/*Startet Song bei bestimmtem Index und spielt ihn ab*/
-    playlist.get(Index);
-
-}
+    public String name;
+    public ArrayList<Song> playlist;
+    public Playlist(String n)
+    {
+        playlist=new ArrayList<Song>();
+        name=n;
+    }
+    public void Play(int Index){/*Startet Song bei bestimmtem Index und spielt ihn ab*/
+        playlist.get(Index);
+    }
 }
