e72e9cb63dad20fb5274f75aeb3fa1e126e865f7
Alex <@>
2025-06-16 18:39:10 +0200

    For loop does not work

--- src/main/java/com/example/walter/Playlist.java
+++ src/main/java/com/example/walter/Playlist.java
@@ -5,12 +5,26 @@ import java.util.ArrayList;
 public class Playlist {
 
     public String name;
+    public ArrayList<String> playliststr;
     public ArrayList<Song> playlist;
+    private ArrayList<String> papa;
+    private Song song = null;
 
 
-    public Playlist(String n)
 
+    public Playlist(int status, String n)
     {
+        if(status == 0){
+            fileReader papaReader = new fileReader();
+            papaReader.read(songinit.txt);
+            for(int  i = 0, i < papaReader.byteStash.size(), i++){
+
+
+
+            }
+
+        }
+
 
         playlist=new ArrayList<Song>();
         name=n;
@@ -18,4 +32,23 @@ public class Playlist {
     public void Play(int Index){
         playlist.get(Index);
     }
+    private void AddSong(String str){
+
+        if(playliststr.contains(str)){
+
+            return;
+
+        }else{
+            SongConvert(str);
+            playliststr.add(str);
+            playlist.add(song);
+
+        }
+    }
+    public void SongConvert(String str){
+
+
+    }
+
+
 }
