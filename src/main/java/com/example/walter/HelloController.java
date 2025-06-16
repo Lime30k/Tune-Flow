eba6950ee6cbd627e91ee3ab92bc89c74fdb64d5
Lime30k <meyerhoff.l@outlook.de>
2025-06-16 16:15:34 +0200

    ViewImpl.java got deleted to make the code more compact. All code got transvered over to HelloController which is the new main class

--- src/main/java/com/example/walter/HelloController.java
+++ src/main/java/com/example/walter/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.walter;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,7 +14,27 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.Priority;
 
-public class HelloController {
+import java.io.IOException;
+
+
+/**
+ DOCUMENTATION: Authors: Alex, Linus, Andreas.
+
+ Usage of this Tune-Flow application:
+
+ Start the app by running the main method.
+ Decide whether you want to listen to an already defined Playlist or a new Song.
+ Search for new songs in the Search engine.
+ Follow your instinct, it might be self-explanatory.
+ Have fun!!!
+
+
+
+ */
+
+
+
+public class HelloController extends Application {
     @FXML
     private Label welcomeText;
     public Button HelloButton;
@@ -27,7 +50,9 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         System.out.println("Label click works!");
-        addSongItem("new song would look like this");
+        summonTheMightyHBox("new song would look like this");
+        summonTheMightyHBox("or this");
+        summonTheMightyHBox("the song of doom!!!");
     }
     @FXML
     protected void onPlayPauseClick()
@@ -41,16 +66,26 @@ public class HelloController {
         }
     }
 
-    protected void addSongItem(String labelText) {
+    @FXML
+    protected void summonTheMightyHBox(String labelText) {
+        //resize vBox just in case
+        songmenubar.setFillWidth(true);
+
         // Create a new HBox
         HBox hbox = new HBox();
-        hbox.setSpacing(10); // optional
+        hbox.setSpacing(10);
         hbox.setPrefHeight(25);
 
+
         Label label = new Label(labelText);
+        HBox.setHgrow(label,Priority.ALWAYS);
+        label.setWrapText(false);
+        label.setEllipsisString("...");
+
 
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
+
 
         Button button = new Button("Button");
 
@@ -67,6 +102,19 @@ public class HelloController {
     }
 
     protected void createPlaylist() {
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("Walter!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 
 }
