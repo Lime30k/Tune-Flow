ecbec4abbd6b79395b636230b3fdd44230d002e5
Alex <@>
2025-06-30 13:19:13 +0200

    Searchbar for songs fixed

--- src/main/java/com/example/walter/HellController.java
+++ src/main/java/com/example/walter/HellController.java
@@ -120,8 +120,66 @@ public class HellController extends Application {
 
     @FXML
     protected void on_search_clicked(){
-        System.out.println("search lol");
+        //System.out.println("search lol");
+        songmenubar.getChildren().clear();
+
+        String str = searchField.getText();
+        System.out.println(str);
+
+        for(int i = 0; i < playlistPapa.playlist.size(); i++){
+            System.out.println(playlistPapa.playlist.get(i).getName());
+            if(calculateCloseness(playlistPapa.playlist.get(i).getName(), str) >= 80){
+
+                summonTheMightyHBox(playlistPapa.playlist.get(i));
+            }
+
+
+        }
     }
+    // Methode zur Berechnung der Levenshtein-Distanz
+    public static int levenshteinDistance(String str1, String str2) {
+        int lenStr1 = str1.length();
+        int lenStr2 = str2.length();
+
+        int[][] dp = new int[lenStr1 + 1][lenStr2 + 1];
+
+        // Fülle die Basisfälle
+        for (int i = 0; i <= lenStr1; i++) {
+            for (int j = 0; j <= lenStr2; j++) {
+                if (i == 0) {
+                    dp[i][j] = j; // Wenn str1 leer ist, sind die Operationen nur Einfügungen
+                } else if (j == 0) {
+                    dp[i][j] = i; // Wenn str2 leer ist, sind die Operationen nur Löschungen
+                } else {
+                    int cost = (str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0 : 1;
+
+                    // Berechne die minimale Anzahl an Operationen (Einfügen, Löschen, Ersetzen)
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + cost);
+                }
+            }
+        }
+
+        return dp[lenStr1][lenStr2]; // Der Wert in der unteren rechten Ecke ist die Levenshtein-Distanz
+    }
+
+    // Methode zur Berechnung des Ähnlichkeitswerts (0 bis 100)
+    public static int calculateCloseness(String str1, String str2) {
+        int distance = levenshteinDistance(str1, str2);
+
+        // Berechne den maximalen Abstand
+        int maxLength = Math.max(str1.length(), str2.length());
+
+        // Wenn beide Strings leer sind, ist die Nähe 100%
+        if (maxLength == 0) {
+            return 100;
+        }
+
+        // Skaliere die Levenshtein-Distanz zu einem Wert von 0 bis 100
+        double similarity = (1.0 - (double) distance / maxLength) * 100;
+
+        return (int) similarity; // Rückgabe als ganzzahliger Wert
+    }
+
 
     protected void hellishSongInitializer(){
         for(int i=0; i<playlistPapa.playlist.size();i++){
@@ -143,6 +201,9 @@ public class HellController extends Application {
         controller.playlistPapa = new Playlist(0, "default");
         System.out.println(controller.playlistPapa.playlist.size());
         controller.hellishSongInitializer();
+
+
+
 
     }
 
